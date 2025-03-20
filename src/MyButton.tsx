48c037107b01af36f4d1c38090fe81f384dc411a
Андрей Родин <a.rodin@gt-sot.ru>
2025-03-20 21:56:57 +0300

    update

--- src/MyButton.tsx
+++ src/MyButton.tsx
@@ -9,7 +9,15 @@ const MyButton: React.FC<ButtonProps> = ({ label, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      style={{
+        padding: "10px 30px",
+        fontSize: "16px",
+        cursor: "pointer",
+        borderRadius: "5px",
+        border: "none",
+        backgroundColor: "green",
+        color: "white",
+      }}
     >
       {label}
     </button>
