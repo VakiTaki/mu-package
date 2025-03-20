3050bbdb7695e75facb7354149cf2f55675839c8
Андрей Родин <a.rodin@gt-sot.ru>
2025-03-20 20:36:22 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react(), dts()],
   build: {
     lib: {
-      entry: "src/index.ts",
-      name: "MyReactLib",
-      fileName: (format) => `my-package.${format}.js`,
+      entry: "src/main.tsx",
+      name: "mu-package",
+      fileName: (format) => `mu-package.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
