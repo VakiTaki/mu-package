9fc46df50d9e3b569a09c0c6927cdcc75842acc2
Андрей Родин <a.rodin@gt-sot.ru>
2025-03-20 22:00:46 +0300

    fix

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import MyButton from "./MyButton";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <MyButton label={"Тест кнопки"} onClick={() => console.log("here")} />
-  </StrictMode>
-);
