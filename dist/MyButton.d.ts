017f49afb597e2adc4b341857ba01af69cabe5ac
Андрей Родин <a.rodin@gt-sot.ru>
2025-03-20 21:21:38 +0300

    hot

--- dist/MyButton.d.ts
+++
@@ -1,7 +0,0 @@
-import { default as React } from 'react';
-type ButtonProps = {
-    label: string;
-    onClick?: () => void;
-};
-declare const MyButton: React.FC<ButtonProps>;
-export default MyButton;
